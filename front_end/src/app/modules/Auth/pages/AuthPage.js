2617c965998e3152b660d313f0ac55cbc95677c9
maxim <maxim.huryanov@appetizermobile.com>
2021-07-22 01:02:22 -0500

    initialize

---
+++ front_end/src/app/modules/Auth/pages/AuthPage.js
@@ -0,0 +1,110 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import { Link, Switch, Redirect } from "react-router-dom";
+import { toAbsoluteUrl } from "../../../../_metronic/_helpers";
+import { ContentRoute } from "../../../../_metronic/layout";
+import Login from "./Login";
+import Registration from "./Registration";
+import ForgotPassword from "./ForgotPassword";
+import "../../../../_metronic/_assets/sass/pages/login/classic/login-1.scss";
+
+export function AuthPage() {
+  return (
+    <>
+      <div className="d-flex flex-column flex-root">
+        {/*begin::Login*/}
+        <div
+          className="login login-1 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid bg-white"
+          id="kt_login"
+        >
+          {/*begin::Aside*/}
+          <div
+            className="login-aside d-flex flex-row-auto bgi-size-cover bgi-no-repeat p-10 p-lg-10"
+            style={{
+              backgroundColor:'#112233',
+            }}
+          >
+            {/*begin: Aside Container*/}
+            <div className="d-flex flex-row-fluid flex-column justify-content-between">
+              {/* start:: Aside header */}
+              {/* end:: Aside header */}
+
+              {/* start:: Aside content */}
+              <div className="flex-column-fluid d-flex flex-column justify-content-center  algin-center" style={{alignItems:"center"}}>
+                <img
+                  alt="Logo"
+                  className="max-h-70px"
+                  style={{
+                    maxWidth:476,
+                  }}
+                  src={toAbsoluteUrl("/media/logos/logo-letter-1.png")}
+                />
+              </div>
+              {/* end:: Aside content */}
+
+              {/* start:: Aside footer for desktop */}
+              <div className="d-none flex-column-auto d-lg-flex justify-content-between mt-10">
+                <div className="d-flex">
+                <p className="font-weight-lighter text-white opacity-80">
+                  Pulitzer Ce nter on Crisis Reporting – Fellow/Correspondent. Traveled across Congo for several weeks to report on election developments, and to raise awareness of the Congo conflict in US media. Embedded with Moroccan, Pakistani and Uruguayan United Nations peacekeepers in Ituri, Lake Albert and South Kivu. Accredited with Ministry of Information and United Nations Mission in Congo (Summer 2006). ntary report on the relation between the Congo conflict and the scramble for mineral resources (Fall 2006). Aired on PBS’ Foreign Exchange with Fareed Zakaria. Guest appearances on BBC’s World News
+                </p>
+                </div>
+              </div>
+              {/* end:: Aside footer for desktop */}
+            </div>
+            {/*end: Aside Container*/}
+          </div>
+          {/*begin::Aside*/}
+
+          {/*begin::Content*/}
+          <div className="d-flex flex-column flex-row-fluid position-relative p-7 overflow-hidden" style={{backgroundColor:"#93999F"}}>
+            {/*begin::Content header*/}
+            <div className="position-absolute top-0 right-0 text-right mt-5 mb-15 mb-lg-0 flex-column-auto justify-content-center py-5 px-10">
+              <span className="font-weight-bold text-dark-50">
+                Don't have an account yet?
+              </span>
+              <Link
+                to="/auth/registration"
+                className="font-weight-bold ml-2"
+                id="kt_login_signup"
+              >
+                Sign Up!
+              </Link>
+            </div>
+            {/*end::Content header*/}
+
+            {/* begin::Content body */}
+            <div className="d-flex flex-column-fluid flex-center mt-30 mt-lg-0">
+              <Switch>
+                <ContentRoute path="/auth/login" component={Login} />
+                <ContentRoute
+                  path="/auth/registration"
+                  component={Registration}
+                />
+                <ContentRoute
+                  path="/auth/forgot-password"
+                  component={ForgotPassword}
+                />
+                <Redirect from="/auth" exact={true} to="/auth/login" />
+                <Redirect to="/auth/login" />
+              </Switch>
+            </div>
+            {/*end::Content body*/}
+
+            {/* begin::Mobile footer */}
+            <div className="d-flex d-lg-none flex-column-auto flex-column flex-sm-row justify-content-between align-items-center mt-5 p-5">
+              <div className="d-flex">
+                <p className="font-weight-lighter opacity-80" style={{borderColor:"#384553",color:"#ACBEE2"}}>
+                  Pulitzer Ce nter on Crisis Reporting – Fellow/Correspondent. Traveled across Congo for several weeks to report on election developments, and to raise awareness of the Congo conflict in US media. Embedded with Moroccan, Pakistani and Uruguayan United Nations peacekeepers in Ituri, Lake Albert and South Kivu. Accredited with Ministry of Information and United Nations Mission in Congo (Summer 2006). ntary report on the relation between the Congo conflict and the scramble for mineral resources (Fall 2006). Aired on PBS’ Foreign Exchange with Fareed Zakaria. Guest appearances on BBC’s World News
+                </p>
+                </div>
+            </div>
+            {/* end::Mobile footer */}
+          </div>
+          {/*end::Content*/}
+        </div>
+        {/*end::Login*/}
+      </div>
+    </>
+  );
+}
